4725bd2e6fa18837ef6f3a0f3c92af4217aeb2ff
Luis De la Cruz <98129640+cruzmediaorg@users.noreply.github.com>
2026-01-12 17:05:13 +0100

    adjust import

--- app/Http/Controllers/ConfigurationController.php
+++ app/Http/Controllers/ConfigurationController.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Enums\Permission;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -14,7 +14,7 @@ final class ConfigurationController extends Controller
     public function index(Request $request): Response
     {
         abort_unless($request->user()->can(Permission::ConfigurationView), 403);
-        
+
         return Inertia::render('configuration/index');
     }
 }
