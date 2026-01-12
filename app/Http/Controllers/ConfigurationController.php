e80b36f48072ef771a19d7994e9c95311f5873bf
Luis De la Cruz <98129640+cruzmediaorg@users.noreply.github.com>
2026-01-12 17:04:21 +0100

    checks permisisons

--- app/Http/Controllers/ConfigurationController.php
+++ app/Http/Controllers/ConfigurationController.php
@@ -4,13 +4,17 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Enums\Permission;
+use Illuminate\Support\Facades\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
 final class ConfigurationController extends Controller
 {
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        abort_unless($request->user()->can(Permission::ConfigurationView), 403);
+        
         return Inertia::render('configuration/index');
     }
 }
